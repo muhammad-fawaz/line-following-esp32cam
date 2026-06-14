7d2aa8640e38358626fddf00250d394024b3c45d
Muhammad Fawaz <muhammadfawaz382@gmail.com>
2026-06-15 01:49:38 +0900

    initial work on PID and motor control

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,41 @@
 
 using eloq::camera;
 
-// Defining Variables
+// Vision Variables
 int width;
 int height;
-int offset;
 int dynamicThreshold;
 
-int targetRow = 90;
+int targetRow[] = {10, 90, 110};
 
+// TODO
+//PID variables
+float kp_pos = 0;
+float kp_ang = 0;
+float ki = 0;
+float kd = 0;
+
+float e = 0;
+float errorIntegral = 0;
+float errorDerivative = 0;
+float currentError = 0;
+float lastError = 0;
+
+
+// Function Definitions
 int calculate_Threshold(uint8_t* buffer, int totalPixels);
-int calculate_Offset(uint8_t* pixelBuffer, int dynamicThreshold);
+int calculate_positionOffset(uint8_t* pixelBuffer, int dynamicThreshold, int targetRow);
+int calculate_AngleOffset(uint8_t* pixelBuffer, int dynamicThreshold);
+void turn_motors(float error);
+float run_obstacle_detection();
+void run_obstacle_maneuver();
+
 
 void setup() {
 
   Serial.begin(115200);
   delay(2000);
-  Serial.println("\n--- Starting ESP32-CAM Line-Tracking Test ---");
+  Serial.println("\n--- Starting ESP32-CAM Line-Tracking (With PID) Test ---");
 
   camera.pinout.wrover();
   camera.brownout.disable();
@@ -41,6 +60,7 @@ void setup() {
 
 void loop() {
   
+  // Checking if camera is working
   while (!camera.capture().isOk()) {
     Serial.println("Failed to Capture Frame: ");
     delay(2000);
@@ -49,22 +69,38 @@ void loop() {
   uint8_t* pixelBuffer = camera.frame->buf;
   int totalPixels = width * height;
 
+  // Calculating threshold value to dynamically classify black and white
   dynamicThreshold = calculate_Threshold(pixelBuffer, totalPixels);
-  offset = calculate_Offset(pixelBuffer, dynamicThreshold);
-
-  Serial.printf("Offset Value: %d\n", offset);
   
+  int offsetPosition = calculate_positionOffset(pixelBuffer, dynamicThreshold, targetRow[1]);
+  float offsetAngle = calculate_AngleOffset(pixelBuffer, dynamicThreshold);
+
+  // PID
+  currentError = offsetPosition*kp_pos + offsetAngle*kp_ang;
+
+  errorIntegral += currentError;
+  errorDerivative = currentError - lastError;
+
+  float e = currentError + (errorIntegral*ki) + (errorDerivative*kd);
+  turn_motors(e);
+
+  lastError = currentError;
+
+  // Logging results for debugging - remove this at the end
+  Serial.printf("PosErr: %d | AngErr: %.1f | TurnOut: %.1f\n", offsetPosition, offsetAngle, e);  
   camera.free();    
   delay(20); 
 }
 
 
-int calculate_Offset(uint8_t* pixelBuffer, int dynamicThreshold) {
+// Check if line is off-centered from middle of the frame
+int calculate_positionOffset(uint8_t* pixelBuffer, int dynamicThreshold, int targetRow) {
   
   long sumPositions = 0;
   int whitePixelCount = 0;
   int startIndex = targetRow * width;
 
+  // Count how many pixels are white and get their position
   for (int x = 0; x < width; x++) {
     int pixelIndex = (startIndex) + x;
     uint8_t pixelValue = pixelBuffer[pixelIndex];
@@ -75,6 +111,7 @@ int calculate_Offset(uint8_t* pixelBuffer, int dynamicThreshold) {
     }
   }
 
+  // Get average position of white pixels
   if (whitePixelCount > 0) {
     int line_centroid_x = sumPositions / whitePixelCount;
     int camera_midpoint_x = width / 2; 
@@ -84,19 +121,57 @@ int calculate_Offset(uint8_t* pixelBuffer, int dynamicThreshold) {
   }
 
   return 0;
-
 }
 
 
-int calculate_Threshold(uint8_t* buffer, int totalPixels) {
+// Angle offset from center used to control PID algorithm
+int calculate_AngleOffset(uint8_t* pixelBuffer, int dynamicThreshold) {
+
+  int top_YOffset = targetRow[1] - targetRow[0];
+  int bottom_YOffset = targetRow[2] = targetRow[1];
+
+  int top_XOffset = calculate_positionOffset(pixelBuffer, dynamicThreshold, targetRow[0]);
+  int bottom_XOffset = calculate_positionOffset(pixelBuffer, dynamicThreshold, targetRow[2]);
+
+  //angle = dY / dX
+  int angle = (top_YOffset + bottom_YOffset) / (top_XOffset + bottom_XOffset);
+  return angle;
+}
+
+
+// Instead of statically defining the threshold once, define it as the average of all pixels on frame
+int calculate_Threshold(uint8_t* pixelBuffer, int totalPixels) {
+
   long thresholdSum = 0;
   
   for (int i = 0; i < totalPixels; i++) {
-    thresholdSum += buffer[i];
+    thresholdSum += pixelBuffer[i];
   }
   
   int frameAverage = thresholdSum / totalPixels;
   int calculatedThreshold = frameAverage;
   
   return calculatedThreshold;
+}
+
+
+//TODO
+// Motor control algorithm based off of PID
+void turn_motors(float error) {
+  return;
+}
+
+
+//TODO
+// Detect objects in frint of robot
+float run_obstacle_detection() {
+  return;
+}
+
+//TODO
+// It is a hassle to make complex code for this,
+// Make a predefined block of code that makes the robot go clckwise around a circle
+// avoiding  the obstacle (initial idea)
+void run_obstacle_maneuver() {
+  return;
 }
